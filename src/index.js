b946f6a4d67165aa04342da1f3dd144f5f2655c9
Dmitriy S <sutyginds@gmail.com>
2020-03-02 00:02:59 +0400

    init: done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+    let result = []
+    if (matrix == null) {
+        return []
+    }
+    for (i=0; i<matrix.length; i++) {
+
+        if ((i+1)%2==0) {
+            temp = matrix[i];
+            result = result.concat(temp.reverse());
+        } else {
+            result = result.concat(matrix[i]);
+        }
+    }
+
+  return result;
 }
+
+// function compareNumeric(a, b) {
+//     if (a > b) return 1;
+//     if (a == b) return 0;
+//     if (a < b) return -1;
+//   }
